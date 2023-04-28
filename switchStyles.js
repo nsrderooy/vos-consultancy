e99d6c15c31d269c95bc6ec89b61fa4e0b0158d7
Norbert de Rooy <nsrderooy@gmail.com>
2023-04-28 13:43:54 +0800

    initial commit, working website

---
+++ switchStyles.js
@@ -0,0 +1,29 @@
+const switchButton = document.getElementById('switch-button');
+const stylesheet = document.getElementById('stylesheet');
+
+// Load the saved style from localStorage, or use the default
+let currentStyle = localStorage.getItem('style');
+if (!currentStyle) {
+  currentStyle = 'styleDark.css';
+}
+stylesheet.href = currentStyle;
+
+// Set the initial state of the switch button
+if (currentStyle === 'styleDark.css') {
+  switchButton.textContent = 'Switch to Light';
+} else {
+  switchButton.textContent = 'Switch to Dark';
+}
+
+// Switch the stylesheet and update the switch button text when clicked
+switchButton.addEventListener('click', () => {
+  if (currentStyle === 'styleDark.css') {
+    currentStyle = 'styleLight.css';
+    switchButton.textContent = 'Switch to Dark';
+  } else {
+    currentStyle = 'styleDark.css';
+    switchButton.textContent = 'Switch to Light';
+  }
+  stylesheet.href = currentStyle;
+  localStorage.setItem('style', currentStyle);
+});
